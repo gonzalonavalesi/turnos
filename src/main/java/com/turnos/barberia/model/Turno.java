3acfe46bc7c3ca042fc1e292518ef75b35eae931
gonzalonavalesi <gonzalonavalesi@gmail.com>
2026-09-04 12:30:45 -0300

    creacion model turno

---
+++ src/main/java/com/turnos/barberia/model/Turno.java
@@ -0,0 +1,29 @@
+package com.turnos.barberia.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "turnos")
+@Setter
+@Getter
+
+public class Turno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaHora;
+
+    private Boolean pago;
+
+}
